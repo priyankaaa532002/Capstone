772171baa80b152437f4384056e318f68e90bb0b
Priyanka <priyanka.chowdhury2020@vitstudent.ac.in>
2024-02-04 12:37:38 +0530

    bug fixing

--- capstone-project/src/components/Doctors.js
+++ capstone-project/src/components/Doctors.js
@@ -1,14 +1,10 @@
 import React, { useContext, useEffect, useState } from 'react';
-import AdminContext from '../MyContext';
+import AdminContext from '../MyContext'; 
 import { CONTRACT_ADDRESS_DOCTOR, ABI_DOCTOR } from '../Constants';
 import { ethers } from 'ethers';
 import Web3 from 'web3';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import TimePicker from 'react-time-picker';
-import 'react-time-picker/dist/TimePicker.css'; // Import styles
-
-
 
 const myStyle = {
     textAlign: "left",
@@ -16,11 +12,13 @@ const myStyle = {
 };
 
 const DoctorManagement = () => {
-    const { isAdmin, patientData } = useContext(AdminContext);
+    const { isAdmin , patientData } = useContext(AdminContext);
     const [doctors, setDoctors] = useState([]);
     const [isFormOpen, setIsFormOpen] = useState(false);
-    const [formData, setFormData] = useState({ name: '', specialty: '', charge: '', account: '' });
+    const [isFormOpenApp, setIsFormOpenApp] = useState(false);
+    const [formData, setFormData] = useState({ name: '', specialty: '', charge: '', account: ''});
     const [searchQuery, setSearchQuery] = useState('');
+
     const [selectedDoctor, setSelectedDoctor] = useState(null);
     const [selectedDate, setSelectedDate] = useState(null);
     const [selectedTime, setSelectedTime] = useState('');
@@ -28,6 +26,7 @@ const DoctorManagement = () => {
     const handleTimeChange = (e) => {
         setSelectedTime(e.target.value);
     };
+
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         setFormData({ ...formData, [name]: value });
@@ -41,9 +40,15 @@ const DoctorManagement = () => {
         }
     };
 
-    const handleAppointment = (doctor) => {
-        setSelectedDoctor(doctor);
-        setIsFormOpen(true);
+    const handleConfirmAppointment = () => {
+        // Implement logic to confirm appointment
+        console.log("Appointment confirmed!");
+        // Reset appointment form fields
+        setSelectedDoctor(null);
+        setSelectedDate(null);
+        setSelectedTime(null);
+        setIsFormOpenApp(false);
+        alert("Appointment confirmed")
     }
 
     async function registerDoctorData() {
@@ -69,7 +74,7 @@ const DoctorManagement = () => {
         e.preventDefault();
         registerDoctorData();
         setFormData({ name: '', specialty: '', charge: '', account: '' });
-        setIsFormOpen(false);
+        setIsFormOpen(false); 
     };
 
     useEffect(() => {
@@ -97,79 +102,29 @@ const DoctorManagement = () => {
         doctor[1].toLowerCase().includes(searchQuery.toLowerCase())
     );
 
-    const handleConfirmAppointment = () => {
-        // Implement logic to confirm appointment
-        console.log("Appointment confirmed!");
-        // Reset appointment form fields
-        setSelectedDoctor(null);
-        setSelectedDate(null);
-        setSelectedTime(null);
-        setIsFormOpen(false);
-    }
+    const handleAppointment = (doctor) => {
+        if(!isAdmin){
+            setIsFormOpenApp(true); //yahan kuch prob ho raha acc to me
 
-    async function convertINRtoETH(amountINR) {
-        try {
-            const response = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=inr`);
-            const data = await response.json();
-
-            if (data.ethereum && data.ethereum.inr) {
-                const ethPriceInINR = data.ethereum.inr;
-                const amountETH = amountINR / ethPriceInINR;
-                return amountETH;
-            } else {
-                throw new Error('Failed to fetch ETH price in INR');
-            }
-        } catch (error) {
-            console.error('Erroor converting INR to ETH:', error);
-            return null;
+            setSelectedDoctor(doctor);
+            // alert(patientData) // ye alert kar raha 
+            // alert(doctor)
         }
+
+        else alert("Only for patients")
+        // setIsFormOpenApp(true);
     }
+
     return (
         <div>
-            {!isFormOpen && (
-                <div style={{ margin: '20px' }}>
-                    <h2 style={{ marginBottom: '20px' }}>Doctors Information</h2>
-                    <div className="mb-3">
-                        <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Search by name or specialty..."
-                            value={searchQuery}
-                            onChange={(e) => setSearchQuery(e.target.value)}
-                        />
-                    </div>
-                    <table className="table table-striped" style={{ borderCollapse: 'collapse', width: '100%' }}>
-                        <thead>
-                            <tr>
-                                <th style={{ padding: '10px' }}>NAME</th>
-                                <th style={{ padding: '10px' }}>SPECIALITY</th>
-                                <th style={{ padding: '10px' }}>CONSULTATION CHARGE</th>
-                                <th style={{ padding: '10px' }}>ACCOUNT ADDRESS</th>
-                                <th style={{ padding: '10px' }}> </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {filteredDoctors.map((doctor, index) => (
-                                <tr key={index}>
-                                    <td style={{ padding: '10px' }}>{doctor[0]}</td>
-                                    <td style={{ padding: '10px' }}>{doctor[1]}</td>
-                                    <td style={{ padding: '10px' }}>{doctor[2].toNumber()}</td>
-                                    <td style={{ padding: '10px' }}>{doctor[3]}</td>
-                                    <td><button type="button" className="btn btn-primary" onClick={handleAppointment.bind(this, doctor)}>Book Appointment</button></td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            )}
-            {isFormOpen && (
+            {isFormOpenApp && (
                 <div className="modal-container d-flex" style={myStyle}>
-                    <div className="modal-background" onClick={() => setIsFormOpen(false)}></div>
+                    <div className="modal-background" onClick={() => setIsFormOpenApp(false)}></div>
                     <div className="modal-dialog">
-                        <div className="modal-content" style={{ border: '1px solid #ccc', width: '400px', margin: '50px auto', padding: '20px' }}>
+                        <div className="modal-content" style={{ border: '1px solid #ccc', width: '400px', margin: 'auto', padding: '20px' }}>
                             <div className="modal-header">
                                 <h5 className="modal-title">Book Appointment</h5>
-                                <button type="button" className="btn-close" aria-label="Close" onClick={() => setIsFormOpen(false)}></button>
+                                <button type="button" className="btn-close" aria-label="Close" onClick={() => setIsFormOpenApp(false)}></button>
                             </div>
                             <div className="modal-body">
                                 <form onSubmit={handleSubmit}>
@@ -186,7 +141,7 @@ const DoctorManagement = () => {
                                         <input type="text" className="form-control" id="charge" name="charge" value={selectedDoctor ? selectedDoctor[2].toNumber() : ''} disabled />
                                     </div>
                                     <div className="mb-3">
-                                        <label htmlFor="charge1" className="form-label">Consultation Charge in INR</label>
+                                        <label htmlFor="charge1" className="form-label">Consultation Charge in ETH</label>
                                         <input type="text" className="form-control" id="charge1" name="charge1" value={selectedDoctor[2].toNumber() * 0.0000052} disabled />
                                     </div>
                                     <div className="mb-3">
@@ -219,12 +174,81 @@ const DoctorManagement = () => {
                     </div>
                 </div >
             )}
+            {!isFormOpen && !isFormOpenApp && (
+                <div style={{ margin: '20px' }}>
+                    <h2 style={{ marginBottom: '20px' }}>Doctors Information</h2>
+                    <div className="mb-3">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Search by name or specialty..."
+                            value={searchQuery}
+                            onChange={(e) => setSearchQuery(e.target.value)}
+                        />
+                    </div>
+                    <table className="table table-striped" style={{ borderCollapse: 'collapse', width: '100%' }}>
+                        <thead>
+                            <tr>
+                                <th style={{ padding: '10px' }}>NAME</th>
+                                <th style={{ padding: '10px' }}>SPECIALITY</th>
+                                <th style={{ padding: '10px' }}>CONSULTATION CHARGE</th>
+                                <th style={{ padding: '10px' }}>ACCOUNT ADDRESS</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {filteredDoctors.map((doctor, index) => (
+                                <tr key={index}>
+                                    <td style={{ padding: '10px' }}>{doctor[0]}</td>
+                                    <td style={{ padding: '10px' }}>{doctor[1]}</td>
+                                    <td style={{ padding: '10px' }}>{doctor[2].toNumber()}</td>
+                                    <td style={{ padding: '10px' }}>{doctor[3]}</td>
+                                    <td><button type="button" className="btn btn-primary" onClick={handleAppointment.bind(this, doctor)}>Book Appointment</button></td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            )}
             <div style={{ position: 'fixed', bottom: '20px', right: '20px' }}>
                 <button type="button" className="btn btn-primary" onClick={handleAddDoctorClick}>
                     Add New Doctor
                 </button>
             </div>
-        </div >
+            {isFormOpen && isAdmin && (
+                <div className="modal-container d-flex" style={myStyle}>
+                    <div className="modal-background" onClick={() => setIsFormOpen(false)}></div>
+                    <div className="modal-dialog">
+                        <div className="modal-content" style={{ border: '1px solid #ccc', width: '400px', margin: '50px auto', padding: '20px' }}>
+                            <div className="modal-header">
+                                <h5 className="modal-title">Add New Doctor</h5>
+                                <button type="button" className="btn-close" aria-label="Close" onClick={() => setIsFormOpen(false)}></button>
+                            </div>
+                            <div className="modal-body">
+                                <form onSubmit={handleSubmit}>
+                                    <div className="mb-3">
+                                        <label htmlFor="name" className="form-label">Name</label>
+                                        <input type="text" className="form-control" id="name" name="name" value={formData.name} onChange={handleInputChange} required />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="specialty" className="form-label">Specialty</label>
+                                        <input type="text" className="form-control" id="specialty" name="specialty" value={formData.specialty} onChange={handleInputChange} required />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="charge" className="form-label">Consultation Charge</label>
+                                        <input type="text" className="form-control" id="charge" name="charge" value={formData.charge} onChange={handleInputChange} required />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="account" className="form-label">Account Address</label>
+                                        <input type="text" className="form-control" id="account" name="account" value={formData.account} onChange={handleInputChange} required />
+                                    </div>
+                                    <button type="submit" className="btn btn-primary">Submit</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
     );
 };
 
